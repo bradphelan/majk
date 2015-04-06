177fa7de935129faec8eda491400f53bd7e24d2e
Brad Phelan <bradphelan@xtargets.com>
2015-04-06 22:19:27 +0200

    Add more tests and examples

--- tests/src/majktest/Class.fs
+++ tests/src/majktest/Class.fs
@@ -65,3 +65,51 @@ let ``majk should propogate exceptions`` () =
         raisesWith<System.Exception> <@ r.Wait(500) @> (fun e -> <@ e.ToString().Contains msg @>)
 
 
+[<Test>]
+let ``We can lazily create a file`` () =
+        
+
+    let fname = "foo.txt"
+    let count = ref 0
+    System.IO.File.Delete fname
+    let ft = majk {
+        let sw = new System.IO.StreamWriter(fname)
+        sw.Write("foo")
+        sw.Dispose()
+        count.Value <- count.Value + 1
+        return fname
+    }
+
+    test <@ count.Value = 0 @>
+    test <@ System.IO.File.Exists fname |> not @>
+
+    ft.Start().Wait()
+    test <@ count.Value = 1 @>
+    test <@ System.IO.File.Exists fname @>
+
+    // The body should not be run again
+    ft.Start().Wait()
+    test <@ count.Value = 1 @>
+
+    test <@ System.IO.File.Exists fname @>
+
+
+[<Test>]
+let ``We can combine targets`` () =
+
+    let t0 = majk { return "a" }
+    let t1 = majk { return "b" }
+    let t2 = majk { return "c" }
+
+    let c = majk {
+
+        let! v0 = t0
+        let! v1 = t1
+        let! v2 = t2
+
+        return v0 + v1 + v2
+
+    }
+
+    c.Start().Wait()
+    test <@ c.Start().Result = "abc" @>
